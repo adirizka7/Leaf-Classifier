6bc2285568b60656cd45596caf9847d9c9f62010
adirizka7 <adiunity@gmail.com>
2018-06-01 22:57:23 +0700

    model and remove kernel

--- app.py
+++ app.py
@@ -57,15 +57,11 @@ def remove_background(file_location):
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         th, threshed = cv2.threshold(gray, 140, 255, cv2.THRESH_BINARY_INV)
 
-        ## (2) Morph-op to remove noise
-        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (11,11))
-        morphed = cv2.morphologyEx(threshed, cv2.MORPH_CLOSE, kernel)
-
-        ## (3) Find the max-area contour
-        _, cnts, _ = cv2.findContours(morphed, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        ## (2) Find the max-area contour
+        _, cnts, _ = cv2.findContours(threshed, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         cnt = sorted(cnts, key=cv2.contourArea)[-1]
 
-        ## (4) Crop and save it
+        ## (3) Crop and save it
         x,y,w,h = cv2.boundingRect(cnt)
         dst = img[y:y+h, x:x+w]
         
